155abc8bf2d90726901d7c47887edf7e2ece82e6
NidaBoy <daniboytorres99xx@gmail.com>
2025-06-17 00:39:31 -0300

    Adicionando os estilos da pagina

--- client/src/App.js
+++ client/src/App.js
@@ -50,16 +50,18 @@ function App() {
           <div className="content-grid">
             {/* Painel de Input */}
             <div className="input-panel">
-              {/*<JsonInput 
+              <JsonInput 
                 onTableData={handleTableData}
                 onError={handleError}
                 onLoading={handleLoading}
                 loading={loading}
               />
-              */}
+
+              {/*
               {!tableData && !error && (
                 <Examples onExampleSelect={handleTableData} />
               )}
+                  */}
             </div>
 
             {/* Painel de Resultado */}
